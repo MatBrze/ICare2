189289b09298f55bc931a3e379b0475fa0908eb0
MatBrze <mateusz.brzezny@gmail.com>
2019-07-07 17:32:46 +0200

    login, logout & register views and urls added

--- ICare/urls.py
+++ ICare/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ICare_App import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.MainView.as_view(), name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register')
 ]
